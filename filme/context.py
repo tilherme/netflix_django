7e4a5ed4bc47e78ef5eb8589ac68dbcf37fa5da5
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-04-17 14:29:27 -0300

    style: home_film carousel

--- filme/context.py
+++ filme/context.py
@@ -1,9 +1,9 @@
 from .models import *
 
 def list_recent_movie(request):
-    list_film = Film.objects.all().order_by('-creation_date') # order decrescente
+    list_film = Film.objects.all().order_by('-creation_date')[0:8] # order decrescente
     return {'list_recent_movie': list_film}
 
 def list_trending_movie(request):
-    list_film = Film.objects.all().order_by('-preview') # order decrescente
+    list_film = Film.objects.all().order_by('-preview')[0:8] # order decrescente
     return {'list_trending_movie': list_film}
