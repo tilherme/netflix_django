02008e6066115d49e078b784672d321edc1d102d
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-06-08 23:00:16 -0300

    feat: create super user

--- filme/apps.py
+++ filme/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
+
+    def ready(self):
+        from .models import User
+        import os
+        email = os.getenv('EMAIL_ADMIN')
+        password = os.getenv('SENHA_ADMIN')
+
+        user = User.objects.filter(email=email)
+        if not user:
+            user = object.create_superuser(username='admin', email=email, password=password, is_active=True, is_staff=True)
