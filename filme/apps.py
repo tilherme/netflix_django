d086d2dcf39f5d5fd89123068fd7ae5677d34e6c
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-06-11 11:54:20 -0300

    create super user

--- filme/apps.py
+++ filme/apps.py
@@ -1,29 +1,33 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-import os
 
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-    def ready(self):
-        # Conectar ao sinal post_migrate para executar a função após as migrações
-        post_migrate.connect(self.create_admin_user, sender=self)
+    # def ready(self):
+    #     from .models import User 
+    #     import os
 
-    def create_admin_user(self, **kwargs):
-        from .models import User  # Importar o modelo aqui para evitar problemas de importação precoce
+    #     email = os.getenv('EMAIL_ADMIN')
+    #     password = os.getenv('SENHA_ADMIN')
+        
+    #     user = User.objects.filter(email=email)
+
+    #     if not user:
+    #         User.objects.create_superuser(username='admin', email=email, password=password,
+                                                        # is_active=True, is_staff=True)
+    def ready(self):
+        from .models import User 
+        import os
+
         email = os.getenv('EMAIL_ADMIN')
         password = os.getenv('SENHA_ADMIN')
-        
-        if email and password:  # Certifique-se de que os valores não são None
-            user = User.objects.filter(email=email).first()
 
-            if not user:
-                usuario = User.objects.create_superuser(username='admin', email=email, password=password,
+        if not email:
+            email = 'admin@admin.com'
+
+        user = User.objects.filter(email=email)
+
+        if not user:
+            User.objects.create_superuser(username='admin', email=email, password=password,
                                                         is_active=True, is_staff=True)
-                print(f"Superuser {usuario.username} created")
-            else:
-                usuario = user
-                print(f"Superuser with email {email} already exists")
-        else:
-            print("Admin email or password not provided in environment variables")
---
+++ teste2/Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn netflix.wsgi --log-file -
