15cbb18772c6f3c71592018887682375e48c9c68
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-06-17 20:39:20 -0300

    deploy

--- filme/apps.py
+++ filme/apps.py
@@ -4,19 +4,18 @@ class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-    # def ready(self):
-    #     from .models import User 
-    #     import os
+    def ready(self):
+        from .models import User 
+        import os
 
-    #     email = os.getenv('EMAIL_ADMIN')
-    #     password = os.getenv('SENHA_ADMIN')
+        email = os.getenv('EMAIL_ADMIN')
+        password = os.getenv('SENHA_ADMIN')
         
-    #     user = User.objects.filter(email=email)
+        user = User.objects.filter(email=email)
 
-    #     if not user:
-    #         User.objects.create_superuser(username='admin', email=email, password=password,
-                                 
-    #                                                     # is_active=True, is_staff=True)
+        if not user:
+            User.objects.create_superuser(username='admin', email=email, password=password,          
+                                                        is_active=True, is_staff=True)
     # def ready(self):
     #     from .models import User 
     #     import os
