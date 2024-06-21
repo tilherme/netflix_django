a141dcc8a6650714f0b69ad39df53b1c49e8606e
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-06-21 19:23:40 -0300

    deploy 3

--- filme/apps.py
+++ filme/apps.py
@@ -10,12 +10,13 @@ class FilmeConfig(AppConfig):
 
         email = os.getenv('EMAIL_ADMIN')
         password = os.getenv('SENHA_ADMIN')
-
+        print(email, 'EMAIL 1')
         if not email:
             email = 'admin@admin.com'
+        print(email, 'EMAIL 2')
 
         user = User.objects.filter(email=email)
 
         if not user:
-            User.objects.create_superuser(username='admin', email=email, password=password,
+            User.objects.create_superuser(username='admin11', email=email, password=password,
                                                         is_active=True, is_staff=True)
